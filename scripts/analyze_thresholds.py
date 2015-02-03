126a4e3f35378724574a63a6cbc09db5514caab1
Adam Taylor <aktaylor08@gmail.com>
2015-02-03 17:09:25 -0600

    working on graphics!

--- scripts/analyze_thresholds.py
+++ scripts/analyze_thresholds.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import argparse
-from collections import defaultdict
 
 import os
+import math
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
-
-
-def handle_no_advance(thresh, spots):
-    pass
-
-
-def handle_advance(thresh, spots):
-    pass
 
 
 def get_thresholds(fname):
@@ -43,14 +35,16 @@ def get_flops(df, key):
     return flop_times
 
 
-def get_thresh_info(thresh, limit=.6):
+def get_thresh_info(thresh_df):
     keys = []
     trues = []
     falses = []
     totals = []
     pts = []
     pfs = []
-    for k,v in thresh.groupby('key'):
+
+    #get flop information as well as number of true and false ect.
+    for k, v in thresh_df.groupby('key'):
         plt.show()
         counts = v['result'].value_counts()
         if True in counts:
@@ -72,25 +66,28 @@ def get_thresh_info(thresh, limit=.6):
 
     df = pd.DataFrame(data={'true_count' : trues, 'false_count' : falses, 'count' : totals,
                             'true_prop': pts, 'false_prop' : pfs}, index=keys)
-    thresh['last'] = np.NaN
-    print thresh['last']
+    # create the dataframe
+    # now add flop_times
+    thresh_df['last_flop'] = np.NaN
     for i in df.index:
-        flops = get_flops(thresh, i)
-        thresh = add_times(thresh, i, flops)
-        print flops
-    return df
+        flop_locations = get_flops(thresh_df, i)
+        thresh_df = add_times(thresh_df, i, flop_locations)
+    return df, thresh_df
 
 
-def add_times(thresh, key, flops):
-    vals = thresh[thresh['key'] == key]
+def add_times(to_add_df, key, locations):
+    vals = to_add_df[to_add_df['key'] == key]
     cur_time = vals.index.to_series().apply(ts_to_sec)
-    vals['last'] = pd.Series(flops, flops)
-    vals['last'] = vals['last'].apply(func=lambda x: pd.Timestamp(x))
-    vals['last'] = vals['last'].apply(ts_to_sec)
-    vals['last'] = cur_time - vals['last'].ffill()
-    for idx, val in vals['last'].iteritems():
-        thresh.loc['last', idx] = val
-    return thresh
+    data = pd.Series(index=vals.index)
+    for idx in locations:
+        data[idx] = idx
+    data = data.apply(func=lambda x: pd.Timestamp(x))
+    data = data.apply(ts_to_sec)
+    data = data.ffill()
+    for idx, val in data.iteritems():
+        to_add_df.loc[idx, 'last_flop'] = cur_time[idx] - val
+    return to_add_df
+
 
 def ts_to_sec(ts):
     epoch = datetime.datetime.utcfromtimestamp(0)
@@ -103,7 +100,7 @@ def ts_to_sec(ts):
 
 def get_times(fname):
     if not os.path.isfile(fname):
-        print 'The file does not exisit'
+        print 'The file does not exist'
         sys.exit(-1)
     df = pd.read_csv(fname, parse_dates=True, index_col=0)
     no_action = []
@@ -128,14 +125,55 @@ def get_times(fname):
             action.append(pd.to_datetime(time, unit='s'))
     return action, no_action
 
+
+def handle_no_advance(thresh, flops, no_adv):
+    pass
+
+
+def handle_advance(thresh, flops, adv, time_limit=3.0):
+    groups = thresh.groupby('key')
+    for i in adv:
+        last_flops = {}
+        # get the time from the last flop
+        for key, data in groups:
+            idx = data.index.asof(i)
+            if not isinstance(idx, float):
+                time = data.loc[idx, 'last_flop']
+                if not math.isnan(time):
+                    last_flops[key] = time
+
+        # sort and filter
+        x = [(k, v) for k, v in last_flops.iteritems()]
+        x = filter(lambda arg: arg[1] < time_limit, x)
+        x = sorted(x, key=lambda arg: arg[1])
+
+        # report
+        et = i + datetime.timedelta(seconds=3.0)
+        st = i - datetime.timedelta(seconds=3.0)
+        in_limits = thresh.between_time(st, et)
+        print 'Advance:', i
+        if len(x) > 0:
+            for flop in x:
+                print '\t', flop[1], flop[0]
+        else:
+            print 'No flops within {:d}'.time_limit
+        print '\n'
+        print len(in_limits)
+        in_limits[in_limits['key']]
+
+
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Show information on the user marks')
     parser.add_argument('-t', '--thresholds', required=True)
     parser.add_argument('-b', '--bag', required=True)
     args = parser.parse_args()
 
-
     thresh = get_thresholds(args.thresholds)
-    flops = get_thresh_info(thresh)
+    flops, thresh = get_thresh_info(thresh)
     adv, no_adv = get_times(args.bag)
-
+    handle_no_advance(thresh, flops, no_adv)
+    handle_advance(thresh, flops, no_adv)
