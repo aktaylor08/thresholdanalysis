dcbd808d53ab6132c01c1bf4b6090a81adf78248
Adam Taylor <aktaylor08@gmail.com>
2015-02-04 16:42:02 -0600

    Analysis.  Nearly ready to create simple gui mock up

--- scripts/analyze_thresholds.py
+++ scripts/analyze_thresholds.py
@@ -11,6 +11,48 @@ import numpy as np
 import sys
 
 
+class ThreshStatement(object):
+
+    def __init__(self, threshold, stmt_key, score, suggestion=None):
+        self.threshold = threshold
+        self.stmt_key = stmt_key
+        self.score = score
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '{:f} {:s} {:s}'.format(self.score, self.threshold, self.stmt_key)
+
+
+class ThreshInfoStore(object):
+
+    def __init__(self):
+        self._stmt_map = {}
+        self._thresh_map = {}
+        self._graph_map = {}
+        self.thresh_list = []
+
+    def import_data(self, thresholds):
+        for i in thresholds:
+            self.import_thresh(i)
+
+    def import_thresh(self, incoming):
+        self.thresh_list.append(incoming)
+
+        # put it in this map
+        if incoming.stmt_key in self._stmt_map:
+            self._stmt_map[incoming.stmt_key].append(incoming)
+        else:
+            self._stmt_map[incoming.stmt_key] = [incoming]
+
+        # put it in this map
+        if incoming.threshold in self._thresh_map:
+            self._thresh_map[incoming.threshold].append(incoming)
+        else:
+            self._thresh_map[incoming.threshold].append(incoming)
+
+
 def get_thresholds(fname):
     if os.path.isfile(fname):
         df = pd.read_csv(fname, parse_dates=True, index_col=0)
@@ -20,12 +62,11 @@ def get_thresholds(fname):
         sys.exit()
 
 
-def get_flops(df, key):
-    data_spot = df[df['key'] == key]
-    value = data_spot['result'][0]
+def get_series_flops(series):
     flopped = False
     flop_times = []
-    for time, res in data_spot['result'].iteritems():
+    value = series[0]
+    for time, res in series.iteritems():
         if res != value:
             if not flopped:
                 flop_times.append(time)
@@ -33,6 +74,12 @@ def get_flops(df, key):
         else:
             flopped = False
     return flop_times
+
+
+def get_flops(df, key):
+    data_spot = df[df['key'] == key]
+    series = data_spot['result']
+    return get_series_flops(series)
 
 
 def get_thresh_info(thresh_df):
@@ -43,7 +90,7 @@ def get_thresh_info(thresh_df):
     pts = []
     pfs = []
 
-    #get flop information as well as number of true and false ect.
+    # get flop information as well as number of true and false ect.
     for k, v in thresh_df.groupby('key'):
         plt.show()
         counts = v['result'].value_counts()
@@ -58,14 +105,14 @@ def get_thresh_info(thresh_df):
         keys.append(k)
         trues.append(t)
         falses.append(f)
-        totals.append(t+f)
-        pt = (t / (float(t+f)))
-        pf = (f / (float(t+f)))
+        totals.append(t + f)
+        pt = (t / (float(t + f)))
+        pf = (f / (float(t + f)))
         pts.append(pt)
         pfs.append(pf)
 
-    df = pd.DataFrame(data={'true_count' : trues, 'false_count' : falses, 'count' : totals,
-                            'true_prop': pts, 'false_prop' : pfs}, index=keys)
+    df = pd.DataFrame(data={'true_count': trues, 'false_count': falses, 'count': totals,
+                            'true_prop': pts, 'false_prop': pfs}, index=keys)
     # create the dataframe
     # now add flop_times
     thresh_df['last_flop'] = np.NaN
@@ -107,7 +154,7 @@ def get_times(fname):
     action = []
     if 'mark_no_action__data_nsecs' in df.columns:
         idx = df.mark_no_action__data_nsecs.dropna().index
-        vals = df.loc[idx, ['mark_no_action__data_secs',  'mark_no_action__data_nsecs']]
+        vals = df.loc[idx, ['mark_no_action__data_secs', 'mark_no_action__data_nsecs']]
         for _, data in vals.iterrows():
             s = data['mark_no_action__data_secs']
             ns = data['mark_no_action__data_nsecs']
@@ -117,7 +164,7 @@ def get_times(fname):
         print 'No bad marked?'
     if 'mark_action__data_nsecs' in df.columns:
         idx = df.mark_action__data_nsecs.dropna().index
-        vals = df.loc[idx, ['mark_action__data_secs',  'mark_action__data_nsecs']]
+        vals = df.loc[idx, ['mark_action__data_secs', 'mark_action__data_nsecs']]
         for _, data in vals.iterrows():
             s = data['mark_action__data_secs']
             ns = data['mark_action__data_nsecs']
@@ -126,21 +173,94 @@ def get_times(fname):
     return action, no_action
 
 
-def handle_no_advance(thresh, flops, no_adv):
-    pass
+def handle_no_advance(thresh_df, flop_info, no_advances, time_limit=5.0):
+    groups = thresh_df.groupby('key')
+    for marked_time in no_advances:
+        scores = []
+        et = marked_time + datetime.timedelta(seconds=time_limit)
+        st = marked_time - datetime.timedelta(seconds=time_limit)
+        for key, data in groups:
+            idx = data.index.asof(marked_time)
+            needed_info = []
+            if not isinstance(idx, float):
+                before = data.between_time(st, idx)
+                after = data.between_time(idx, et)
+                total = data.between_time(st, et)
+                cols = [x for x in total.columns if x.startswith('res_') and len(total[x].dropna()) > 0]
+                for count, val in enumerate(cols):
+                    try:
+                        info = {'threshold': data['thresholds'].values[0].split(':')[count]}
+
+                        #scale the data to make the distance resonable
+                        comp = before['cmp_{:d}_0'.format(count)].astype('float')
+                        const = before['const_{:d}_0'.format(count)].astype('float')
+                        v1 = comp.max(), comp.min()
+                        v2 = const.max(), const.min()
+                        maxval = max(v1[0], v2[0])
+                        minval = min(v1[1], v2[1])
+                        if maxval - minval != 0:
+                            comp = comp - minval / (maxval - minval)
+                            const = const - minval / (maxval - minval)
+                        else:
+                            comp.loc[:] = .5
+                            const.loc[:] = .5
+
+                        # calculate the distance
+                        dist = comp - const
+                        dist = np.sqrt(dist * dist).mean()
+                        if dist == 0:
+                            dist = 999
+                        info['distance'] = dist
+                        res_series = before['res_{:d}'.format(count)]
+                        flop_in_series = get_series_flops(res_series)
+                        info['flop_count'] = len(flop_in_series)
+                        tp = flop_info.loc[key, 'true_prop']
+                        fp = flop_info.loc[key, 'false_prop']
+
+                        # does it match
+                        if len(flop_in_series) > 0:
+                            info['match'] = True
+                        else:
+                            if tp > fp:
+                                if res_series[0]:
+                                    info['match'] = False
+                                else:
+                                    info['match'] = True
+                            else:
+                                if res_series[0]:
+                                    info['match'] = True
+                                else:
+                                    info['match'] = False
+                        needed_info.append(info)
+                    except ValueError as ve:
+                        print ve
+            for i in needed_info:
+                match_count = 0
+                for j in needed_info:
+                    if j['threshold'] != i['threshold']:
+                        if j['match']:
+                            match_count += 1
+                s = (i['distance'] + i['flop_count']) / (1 + match_count)
+                scores.append((key, i['threshold'], s))
+
+        values = sorted(scores, key=lambda x: x[2])
+        print marked_time
+        for i in values:
+            ts = ThreshStatement(i[1], i[0], i[2])
+            print ts
+        print '\n\n\n'
+    return
 
 
-def handle_advance(thresh, flops, adv, time_limit=8.0):
-    groups = thresh.groupby('key')
-    for marked_time in adv:
+def handle_advance(thresh_info, flops, advances, time_limit=5.0):
+    groups = thresh_info.groupby('key')
+    for marked_time in advances:
         et = marked_time + datetime.timedelta(seconds=time_limit)
         st = marked_time - datetime.timedelta(seconds=time_limit)
         last_flops = {}
         # get the time from the last flop
         for key, data in groups:
             idx = data.index.asof(marked_time)
-
-
 
             if not isinstance(idx, float):
                 if idx > st:
@@ -154,40 +274,70 @@ def handle_advance(thresh, flops, adv, time_limit=8.0):
         x = sorted(x, key=lambda arg: arg[1])
 
         # report
-        in_limits = thresh.between_time(st, et)
+        in_limits = thresh_info.between_time(st, et)
         print 'Advance:', marked_time
         if len(x) > 0:
             for flop in x:
-                values = in_limits[in_limits['key'] == flop[0]]
-                size = len([k for k in values.columns if k.startswith('res_') and len(values[k].dropna() > 1)])
+                windowed_threshold = in_limits[in_limits['key'] == flop[0]]
+                size = len([k for k in windowed_threshold.columns if
+                            k.startswith('res_') and len(windowed_threshold[k].dropna() > 1)])
                 if size > 1:
+                    code_thresh = windowed_threshold['thresholds'].values[0].split(':')
                     fig, ax = plt.subplots(size)
                     for i in range(size):
                         k1 = 'cmp_{:d}_0'.format(i)
                         k2 = 'const_{:d}_0'.format(i)
-                        ax[i].plot(values[k1])
-                        ax[i].plot(values[k2])
-                        a = ax[i].get_ylim()
-                        mid = (a[0] + a[1]) / 2.0
-                        # ax[i].scatter([marked_time], [mid], marker='*', c='g', s=40)
+                        plot_and_analyze(windowed_threshold, k1, k2, marked_time, ax[i], code_thresh[i], st, et)
                     plt.show()
-
                 else:
+                    thresh_name = windowed_threshold['thresholds'].values[0]
                     fig, ax = plt.subplots()
-                    print values['cmp_0_0'].values.head(4)
-                    ax.plot(values['cmp_0_0']) # .index, values['cmp_0_0'].values)
-                    ax.plot(values['const_0_0']) #.index, values['const_0_0'].values)
-                    a = ax.get_ylim()
-                    mid = (a[0] + a[1]) / 2.0
-                    s = pd.Series(data=[mid], index=[marked_time])
-                    ax.scatter(s.index, s.values)
+                    plot_and_analyze(windowed_threshold, 'cmp_0_0', 'const_0_0', marked_time, ax, thresh_name, st, et)
                     plt.show()
-
                 print '\t', size
                 print '\t', flop[1], flop[0], len(in_limits[in_limits['key'] == flop[0]])
 
         else:
             print 'No flops within {:f}'.format(time_limit)
+
+
+def plot_and_analyze(windowed_threshold, k1, k2, marked_time, ax, thresh_name, st, et):
+    # get if the value is high or low
+    # before
+    before = windowed_threshold.between_time(st, marked_time)
+    after = windowed_threshold.between_time(marked_time, et)
+    diff_b = before[k1].astype('float') - before[k2].astype('float')
+    diff_after = after[k1].astype('float') - after[k2].astype('float')
+    diff_b = diff_b.mean()
+    diff_after = diff_after.mean()
+    if diff_b > 0:
+        if diff_after > 0:
+            confidence = 'high'
+            sugestion = 'Raise!'
+        else:
+            confidence = 'low'
+            sugestion = 'raise'
+    else:
+        if diff_after < 0:
+            confidence = 'high'
+            sugestion = 'Lower!'
+        else:
+            confidence = 'low'
+            sugestion = 'lower'
+
+    ax.plot(windowed_threshold.loc[:, k1].index, [float(x) for x in windowed_threshold.loc[:, k1].values],
+            label='Compare Value')
+    ax.plot(windowed_threshold.loc[:, k2].index, [float(x) for x in windowed_threshold.loc[:, k2].values],
+            label=thresh_name)
+    a = ax.get_ylim()
+    mid = (a[0] + a[1]) / 2.0
+    ax_range = a[1] - a[0]
+    ax.set_ylim(a[0] - .05 * ax_range, a[1] + .05 * ax_range)
+    ax.scatter([marked_time], [mid], marker='*', c='g', s=40, label='Marked Action')
+    ax.text(0.95, 0.01, sugestion, verticalalignment='bottom', horizontalalignment='right',
+            transform=ax.transAxes, color='g', fontsize=16)
+
+    ax.legend()
 
 
 if __name__ == '__main__':
@@ -200,4 +350,4 @@ if __name__ == '__main__':
     flops, thresh = get_thresh_info(thresh)
     adv, no_adv = get_times(args.bag)
     handle_no_advance(thresh, flops, no_adv)
-    handle_advance(thresh, flops, no_adv)
+    handle_advance(thresh, flops, adv)
